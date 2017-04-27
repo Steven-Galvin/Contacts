ff7d2210a71f39571cc3e6d9a83fd5c58b9e6c75
Dana Weiss and Steven Galvin <dana@>
2017-04-27 11:59:03 -0700

    add method and pass spec id for email class

--- lib/email.rb
+++ lib/email.rb
@@ -1,8 +1,11 @@
 class Email
-  attr_reader(:email_address, :type)
+  attr_reader(:email_address, :type, :id)
+
+  @@emails = []
 
   define_method(:initialize) do |attributes|
     @email_address = attributes.fetch(:email_address)
     @type = attributes.fetch(:type)
+    @id = @@emails.length().+(1)
   end
 end
--- spec/email_spec.rb
+++ spec/email_spec.rb
@@ -5,15 +5,22 @@ describe Email do
 
   describe('#email_address') do
     it('returns the email address') do
-      test_email = Email.new({:email_address => 'no@no.com', :type => 'Personal'})
+      test_email = Email.new({:email_address => 'no@no.com', :type => 'Personal', :id => 1})
       expect(test_email.email_address()).to(eq('no@no.com'))
     end
   end
 
   describe('#type') do
     it('returns the type of email address') do
-      test_email = Email.new({:email_address => 'no@no.com', :type => 'Personal'})
+      test_email = Email.new({:email_address => 'no@no.com', :type => 'Personal', :id => 1})
       expect(test_email.type()).to(eq('Personal'))
+    end
+  end
+
+  describe('#id') do
+    it('returns id of an email address') do
+      test_email = Email.new({:email_address => 'no@no.com', :type => 'Personal', :id => 1})
+      expect(test_email.id()).to(eq(1))
     end
   end
 end
