866db4bde700fad915f469986a1c83fd240904de
Dana Weiss and Steven Galvin <dana@>
2017-04-27 16:31:13 -0700

    update our app.rb and erb files to fix routing issues.

--- app.rb
+++ app.rb
@@ -1,10 +1,34 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require './lib/contacts'
+require './lib/address'
+require './lib/email'
+require './lib/phone'
 require 'pry'
 
 also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/contacts') do
+  @contacts = Contact.all()
+  erb(:contacts)
+end
+
+get('/contacts/new') do
+  erb(:contacts_form)
+end
+
+post('/contacts') do
+  first_name = params.fetch('first_name')
+  last_name = params.fetch('last_name')
+  job_title = params.fetch('job_title')
+  company = params.fetch('company')
+  @contact = Contact.new({:first_name => first_name, :last_name => last_name, :job_title => job_title, :company => company})
+
+  @contact.save
+  @contacts = Contact.all
+  redirect('/contacts')
 end
