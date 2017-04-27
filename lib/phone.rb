c4b44c7a042b3d01ecfe0b93135d1affae08cc45
Dana Weiss and Steven Galvin <dana@>
2017-04-27 11:29:13 -0700

    add methods and pass specs for finding type and id for phone class

--- lib/phone.rb
+++ lib/phone.rb
@@ -1,8 +1,11 @@
 class Phone
-  attr_reader(:area, :number)
+  attr_reader(:area, :number, :type, :id)
+  @@phone_numbers = []
 
   define_method(:initialize) do |attributes|
     @area = attributes.fetch(:area)
     @number = attributes.fetch(:number)
+    @type = attributes.fetch(:type)
+    @id = @@phone_numbers.length().+(1)
   end
 end
--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -6,15 +6,29 @@ describe Phone do
 
   describe('#area') do
     it('returns area code of phone number') do
-      test_phone = Phone.new({:area => "503", :number => "555-5035"})
+      test_phone = Phone.new({:area => "503", :number => "555-5035", :type => "Home", :type => "Home", :id => 1})
       expect(test_phone.area()).to(eq("503"))
     end
   end
 
   describe('#number') do
     it('returns number of phone number') do
-      test_phone = Phone.new({:area => "503", :number => "555-5035"})
+      test_phone = Phone.new({:area => "503", :number => "555-5035", :type => "Home", :type => "Home", :id => 1})
       expect(test_phone.number()).to(eq("555-5035"))
+    end
+  end
+
+  describe('#type') do
+    it('returns type of phone number') do
+      test_phone = Phone.new({:area => "503", :number => "555-5035", :type => "Home", :type => "Home", :id => 1})
+      expect(test_phone.type()).to(eq("Home"))
+    end
+  end
+
+  describe('#id') do
+    it('returns the id of a phone number') do
+      test_phone = Phone.new({:area => "503", :number => "555-5035", :type => "Home", :type => "Home", :id => 1})
+      expect(test_phone.id()).to(eq(1))
     end
   end
 
