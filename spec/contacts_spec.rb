6f62174eb03a840daf2af70d7377ee56908eda40
Dana Weiss and Steven Galvin <dana@>
2017-04-27 09:27:56 -0700

    add methods and passing tests for job title and company of contact class

--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -6,15 +6,27 @@ require "pry"
 
     describe('#first_name') do
       it('returns the first name of a contact') do
-        test_contact = Contact.new({:first_name => "Dana", :last_name => "Weiss"})
+        test_contact = Contact.new({:first_name => "Dana", :last_name => "Weiss", :job_title => "Software Developer", :company => "Epicodus"})
         expect(test_contact.first_name()).to(eq("Dana"))
       end
     end
 
     describe('#last_name') do
       it('returns the last name of a contact') do
-        test_contact = Contact.new({:first_name => "Dana", :last_name => "Weiss"})
+        test_contact = Contact.new({:first_name => "Dana", :last_name => "Weiss", :job_title => "Software Developer", :company => "Epicodus"})
         expect(test_contact.last_name()).to(eq("Weiss"))
+      end
+    end
+
+    describe('#job_title') do
+      it('returns the job title of a contact') do
+        test_contact = Contact.new({:first_name => "Dana", :last_name => "Weiss", :job_title => "Software Developer", :company => "Epicodus"})
+      end
+    end
+
+    describe('#company') do
+      it('returns the company of a contact') do
+        test_contact = Contact.new({:first_name => "Dana", :last_name => "Weiss", :job_title => "Software Developer", :company => "Epicodus"})
       end
     end
   end
